b0c754143a949a6af77fa655addd5613048e15c3
stepan41k <russia.zont@gmail.com>
2026-03-11 13:14:33 +0300

    feat: update main.go

---
+++ backend/cmd/billing/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stepan41k/testServer/pkg/api"
+	"github.com/stepan41k/testServer/pkg/repository"
+)
+
+func main() {
+	
+	db := 
+	handler := 
+	service
+	router := chi.NewRouter()
+
+	router.Route("api", func(r chi.Router) {
+		r.Post("/login", )
+		r.Post("/profile")
+	})
+}
