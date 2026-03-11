0fe9ba18caa2d5f0dd24db9393fa9bb7c99a22d7
sudo-odner <odner.main@gmail.com>
2026-03-11 13:10:31 +0300

    delete file & mkdir internal

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"log"
-
-	"github.com/gorilla/mux"
-	"github.com/stepan41k/testServer/pkg/api"
-	"github.com/stepan41k/testServer/pkg/repository"
-)
-
 func main() {
-	db, err := repository.New("postgres://postgres:admin@localhost:5432/cources")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	api := api.New(mux.NewRouter(), db)
-	api.Handle()
-	log.Fatal(api.ListenAndServe("localhost:8090"))
 
 }
