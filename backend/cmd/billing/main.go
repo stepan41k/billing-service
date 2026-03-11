34c60e5792a6a846413ea2a0f647f8ac9b2dba97
sudo-odner <odner.main@gmail.com>
2026-03-11 20:37:04 +0300

    chore: delete old name in /cmd & rebuild go.mod

--- backend/cmd/billing/main.go
+++ backend/cmd/billing/main.go
@@ -2,25 +2,22 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/stepan41k/billing-service/internal/repository/firebird"
-	"github.com/stepan41k/testServer/pkg/api"
-	"github.com/stepan41k/testServer/pkg/repository"
 )
 
 func main() {
-	
+
 	connStr := fmt.Sprintf()
 
 	db := firebird.NewDB()
-	handler := 
-	service
+	handler :=
+		service
 	router := chi.NewRouter()
 
 	router.Route("api", func(r chi.Router) {
-		r.Post("/login", )
+		r.Post("/login")
 		r.Post("/profile")
 	})
 }
