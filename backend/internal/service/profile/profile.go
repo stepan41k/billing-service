643d2c2eb7a382fd6e4d88bac2eac357bef4b28a
sudo-odner <odner.main@gmail.com>
2026-03-18 16:04:06 +0300

    feat: add profile service

--- backend/internal/service/profile/profile.go
+++ backend/internal/service/profile/profile.go
@@ -2,6 +2,7 @@ package profile
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/stepan41k/billing-service/internal/models"
 	"go.uber.org/zap"
@@ -13,22 +14,33 @@ type ProfileRepository interface {
 }
 
 type ProfileService struct {
-	log *zap.Logger
+	log               *zap.Logger
 	profileRepository ProfileRepository
 }
 
 func New(log *zap.Logger, profileRepository ProfileRepository) *ProfileService {
 	return &ProfileService{
-		log: log,
+		log:               log,
 		profileRepository: profileRepository,
-	}	
+	}
 }
 
 func (ps *ProfileService) GetClient(ctx context.Context, login string) (*models.NormalizedClient, error) {
+	const op = "service.profile.GetClient"
+	log := ps.log.With(
+		zap.String("op", op),
+		zap.String("login", login),
+	)
 
+	normalizedClient, err := ps.profileRepository.GetNormalized(ctx, login)
+	if err != nil {
+		log.Error("failed to get client", zap.Error(err))
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return normalizedClient, nil
 }
 
 func (ps *ProfileService) CreateClient(ctx context.Context, newClient models.NewClient) (*models.NormalizedClient, error) {
-
+	return nil, nil
 }
-
