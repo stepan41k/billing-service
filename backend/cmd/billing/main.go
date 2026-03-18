7c256667ecbe8c1fc4a8958a2de7316d73d877c5
stepan41k <russia.zont@gmail.com>
2026-03-18 12:11:15 +0300

    style: remove spaces and change goroutine call

--- backend/cmd/billing/main.go
+++ backend/cmd/billing/main.go
@@ -16,6 +16,7 @@ import (
 
 func main() {
 	cfg := config.MustLoad()
+
 	logger, err := logger.New(logger.Env(cfg.Env))
 	if err != nil {
 		log.Fatalf("failed initilizate logger: %s", err.Error())
@@ -26,7 +27,9 @@ func main() {
 		logger.Fatal("failed to initialize application", zap.Error(err))
 	}
 
-	go application.Run()
+	go func() {
+		application.Run()
+	}()
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
