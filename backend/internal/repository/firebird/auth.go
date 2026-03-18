a733013b782a3bbf5d1a5e168365d8334eed64ea
stepan41k <russia.zont@gmail.com>
2026-03-18 12:10:01 +0300

    feat: add implementation of auth get password method

--- backend/internal/repository/firebird/auth.go
+++ backend/internal/repository/firebird/auth.go
@@ -1,5 +1,42 @@
 package firebird
 
-func (fr *FirebirdRepo) Get() {
-	
+import (
+	"context"
+	"fmt"
+)
+
+func (fr *FirebirdRepo) GetPassword(ctx context.Context, login string) (string, error) {
+	const op = "repository.firebird.auth.GetPassword"
+
+	tx, err := fr.db.BeginTx(ctx, nil)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+
+		commitErr := tx.Commit()
+		if commitErr != nil {
+			err = fmt.Errorf("%s: %w", op, commitErr)
+		}
+	}()
+
+	var password string
+
+	row := fr.db.QueryRowContext(ctx, `
+		SELECT PASSWORD_HASH
+		FROM ACCOUNTS
+		WHERE LOGIN = $1;
+	`, login)
+
+	err = row.Scan(&password)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return password, nil
 }
