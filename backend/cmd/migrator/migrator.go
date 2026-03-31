85fa7518e5640be44c0d4192c537574ad739da99
sudo-odner <odner.main@gmail.com>
2026-03-31 17:27:48 +0300

    test: test

--- backend/cmd/migrator/migrator.go
+++ backend/cmd/migrator/migrator.go
@@ -1,33 +1,58 @@
 package migrator
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/firebird" // Драйвер БД
-	_ "github.com/golang-migrate/migrate/v4/source/file"       // ЭТОГО ИМПОРТА НЕ ХВАТАЛО
+	"github.com/golang-migrate/migrate/v4/database/firebird"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/nakagami/firebirdsql"
-	"github.com/stepan41k/billing-service/internal/config"
+	// "github.com/stepan41k/billing-service/internal/config"
 )
 
 func Migrate() {
-	cfgFireBird := config.MustLoadMigration()
+	// cfgFireBird := config.MustLoadMigration()
 	migrationPath := os.Getenv("MIGRATIONS_PATH")
 	if migrationPath == "" {
 		migrationPath = "/app/migrations"
 	}
 
 	sourceURL := "file://" + migrationPath
-	databaseURL := "firebirdsql://" + cfgFireBird.DSN()
+	// databaseURL := "firebirdsql://" + cfgFireBird.DSN()
 
-	log.Printf("INFO: FireBird URL: %s; source URL: %s", databaseURL, sourceURL)
-	m, err := migrate.New(sourceURL, databaseURL)
+	dsn := "BILLING:masterkey@192.168.1.22:3050/C:/DATABASES/BILLING.FDB?auth_plugin_name=Srp"
+
+	db, err := sql.Open("firebirdsql", dsn)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("CRITICAL: Не удалось открыть базу: %v", err)
+	}
+	defer db.Close()
+
+	driver, err := firebird.WithInstance(db, &firebird.Config{})
+	if err != nil {
+		log.Fatalf("CRITICAL: Ошибка создания драйвера мигратора: %v", err)
+	}
+
+	log.Printf("INFO: Запуск миграций. Источник: %s", sourceURL)
+
+	m, err := migrate.NewWithDatabaseInstance(
+		sourceURL,
+		"firebird",
+		driver,
+	)
+	if err != nil {
+		log.Fatalf("CRITICAL: Ошибка инициализации мигратора: %v", err)
 	}
 
 	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatal(err)
+		log.Fatalf("CRITICAL: Ошибка при выполнении миграции: %v", err)
+	}
+
+	if err == migrate.ErrNoChange {
+		log.Println("INFO: Миграции не требуются (база актуальна)")
+	} else {
+		log.Println("SUCCESS: Миграции успешно применены!")
 	}
 }
