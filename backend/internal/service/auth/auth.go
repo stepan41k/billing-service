fc8c32fa07ee9ebf205ad488b40839ca43d6e3cf
stepan41k <russia.zont@gmail.com>
2026-04-15 00:29:10 +0300

    refactor: remove shadowing err variable

--- backend/internal/service/auth/auth.go
+++ backend/internal/service/auth/auth.go
@@ -55,7 +55,7 @@ func (as *AuthService) Login(ctx context.Context, login, password string) (*mode
 		return nil, nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	if err := bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password)); err != nil {
+	if err = bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password)); err != nil {
 		return nil, nil, domain.ErrInvalidCredentials
 	}
 
